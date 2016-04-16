c567cc31a6ad96544e69f6026677e1fac0f4acfe
Dario Seyb <dario.seyb@gmail.com>
2016-04-16 17:50:40 +0200

    moved stuff around

--- face_recognition.hh
+++ face_recognition.hh
@@ -1,12 +1,1 @@
-#include "opencv2/core/core.hpp"
-#include "opencv2/contrib/contrib.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include <iostream>
-#include <fstream>
-#include <sstream>
-
-using namespace cv;
-using namespace std;
-
-static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';');
 int get_label(int label);
