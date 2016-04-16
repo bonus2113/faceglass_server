0cdf462128016ca9046a78d0fcc6a16c3979be1e
Dario Seyb <dario.seyb@gmail.com>
2016-04-16 17:59:57 +0200

    removed contrib

--- face_recognition.cc
+++ face_recognition.cc
@@ -1,8 +1,6 @@
 #include "face_recognition.hh"
 
 #include "opencv2/core/core.hpp"
-#include "opencv2/contrib/contrib.hpp"
-#include "opencv2/highgui/highgui.hpp"
 #include <iostream>
 #include <fstream>
 #include <sstream>
