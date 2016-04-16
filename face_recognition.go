bdc358e315cf1f42bac51bfaf8ea6c8a76b4bdd2
Dario Seyb <dario.seyb@gmail.com>
2016-04-16 15:02:38 +0000

    fixed file names

--- face_recognition.go
+++ face_recognition.go
@@ -1,17 +1,6 @@
 package main
 
-// typedef int (*intFunc) ();
-//
-// int
-// bridge_int_func(intFunc f)
-// {
-//		return f();
-// }
-//
-// int fortytwo()
-// {
-//	    return 42;
-// }
+// #include "face_recognition.h"
 import "C"
-import "C"
+
 
